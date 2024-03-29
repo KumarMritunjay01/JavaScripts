47120889d36918e2edfe187501869ff4cde1d51d
KumarMritunjay01 <121393993+KumarMritunjay01@users.noreply.github.com>
2024-03-29 04:09:23 +0000

    Comparision

---
+++ 01basic/04comparision.js
@@ -0,0 +1,14 @@
+// console.log(2 > 1)   -> true
+// console.log(2 >= 1) ->  true
+// console.log(2 < 1)  ->  false
+// console.log(2 == 1) ->  false
+// console.log(2 != 1) ->  true
+
+console.log("2">1)  // -> true  due to Js convert in number
+console.log("2"==2) // true
+console.log("2" === 2) // flase -> due to === chake data types
+
+console.log("Hello")
+console.log(null > 0)  // false
+console.log(null == 0) // false
+console.log(null >= 0) // ture
