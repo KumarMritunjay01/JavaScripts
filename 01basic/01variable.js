972179d20ba262414963218c28350d2432fa122a
KumarMritunjay01 <121393993+KumarMritunjay01@users.noreply.github.com>
2024-03-29 02:56:42 +0000

    variable

---
+++ 01basic/01variable.js
@@ -0,0 +1,10 @@
+const accountId = 123443
+let accountEmail= "mritunjya@525"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+
+// accountId = 2  // not allow
+
+accountEmail = "abc"
+accountPassword ="2323"
+accountCity = "Bengaluru"
