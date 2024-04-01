87b80ab9136ab7b8ac25e58daa46c7b25c7ab978
KumarMritunjay01 <121393993+KumarMritunjay01@users.noreply.github.com>
2024-04-01 06:29:13 +0000

    For Loop

---
+++ 05iterations/one.js
@@ -0,0 +1,21 @@
+// ****27*** Lecture-27 ***27**** (Looping)
+
+// for Loop
+
+/*
+for(let index =0; index<=10 ; index++){
+    const ele = index;
+    if(ele == 5){
+        console.log("5 is best number")
+    }
+    console.log(ele)
+}
+*/
+
+// Nasted loop
+
+for(let i=0; i<=10; i++){
+    for(let j=0; j<=10 ; j++){
+        console.log(i,j)
+    }
+}
