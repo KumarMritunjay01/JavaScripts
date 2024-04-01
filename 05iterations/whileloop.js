604594f6f30f8321af85ac65e8794608da01324a
KumarMritunjay01 <121393993+KumarMritunjay01@users.noreply.github.com>
2024-04-01 06:44:29 +0000

    While & doWhile Loop

---
+++ 05iterations/whileloop.js
@@ -0,0 +1,31 @@
+// ****28*** Lecture-28 ***28***
+
+// While loop 
+
+let index = 0;
+
+while(index<=10){
+    // console.log(`value of index is ${index}`)
+    index ++;
+}
+
+// while loop in array
+
+let myArr = [1,2,3,4,5]
+
+let arr =0;
+while (arr < myArr.length){
+    console.log(`value is ${myArr[arr]}`)
+    arr++;
+}
+
+// do while loop
+
+let score = 1;  // if score is 11
+
+do{
+    console.log(`score is ${score}`)
+    score++;
+}while(score<=10);
+
+// do while loop loop must be excute at ones
