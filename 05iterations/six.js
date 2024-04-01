8a39224bc482bc0abc1c2504bc51fbbe795e54fc
KumarMritunjay01 <121393993+KumarMritunjay01@users.noreply.github.com>
2024-04-01 19:19:08 +0000

    Lecture-30 pending

---
+++ 05iterations/six.js
@@ -0,0 +1,17 @@
+// ****30*** Lecture-30 ***30***
+
+// forEach loop
+
+// const coding =  ["js","rb","py","java","python","cpp"]
+
+// const values = coding.forEach((item) => {
+//     console.log(item)
+// })
+
+// console.log(values)
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+const newNums = myNums.filter((num) => num>4)   // [ 5, 6, 7, 8, 9, 10 ]
+
+console.log(newNums)
