deb88b7b7266f955ca3155f20a8716b2a1a10c42
KumarMritunjay01 <121393993+KumarMritunjay01@users.noreply.github.com>
2024-04-03 13:08:51 +0000

    Project02 Update Script file

--- 07projects/project02/script.js
+++ 07projects/project02/script.js
@@ -1,19 +1,22 @@
-const form = document.querySelector('form');
-// console.log(form)
 
-// this useCase will give you empty while using here
+const form = document.querySelector('form');
+// this usecase will give you empty
 // const height = parseInt(document.querySelector('#height').value)
 
-// Event addEventListener
 form.addEventListener('submit', function (e) {
-//   e.preventDefault(); // hold with emidiate submit
+  e.preventDefault();   // hold with emidiate submit
 
   const height = parseInt(document.querySelector('#height').value);
   const weight = parseInt(document.querySelector('#weight').value);
   const results = document.querySelector('#results');
 
-  if (height === '' || height < 0) {
-    // results.innerHTML = 'Plese give a valide height';
-    console.log('Hello');
+  if (height === '' || height < 0 || isNaN(height)) {
+    results.innerHTML = `Please give a valid height ${height}`;
+  } else if (weight === '' || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `Please give a valid weight ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    //show the result
+    results.innerHTML = `<span>${bmi}</span>`;
   }
 });
