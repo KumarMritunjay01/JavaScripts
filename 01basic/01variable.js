5aa93edf092b1aa2ce1dbba7ef1152e1436a6ce0
KumarMritunjay01 <121393993+KumarMritunjay01@users.noreply.github.com>
2024-06-30 12:09:56 +0530

    Update in 01basic/01variable.js/line-no. 6

--- 01basic/01variable.js
+++ 01basic/01variable.js
@@ -3,7 +3,7 @@ let accountEmail= "mritunjya@525"
 var accountPassword = "1234"
 accountCity = "Jaipur"
 
-// accountId = 2  // not allow
+// accountId = 2  // not allow  due to accountId assign as a constant
 
 accountEmail = "abc"
 accountPassword ="2323"
